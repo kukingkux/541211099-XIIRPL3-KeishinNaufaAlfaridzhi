9751aae6b8c88ad9ca3afa599378d5d638f3f676
kukingkux <cbmamangbae@gmail.com>
2023-10-25 21:45:20 +0700

    menambahkan data

--- controllers/users.js
+++ controllers/users.js
@@ -21,15 +21,19 @@ module.exports = {
       res.status(400).json({ success: false });
     }
   },
-  store: (req, res) => {
-    users.push(req.body);
-    res.json({
-      status: true,
-      data: users,
-      method: req.method,
-      url: req.url,
-      message: "Data berhasil ditambah",
-    });
+  store: async (req, res) => {
+    try {
+      const user = await User.create(req.body);
+      res.status(200).json({
+        status: true,
+        data: user,
+        method: req.method,
+        url: req.url,
+        message: "Data berhasil ditambahkan",
+      });
+    } catch (error) {
+      res.status(400).json({ success: false });
+    }
   },
   update: (req, res) => {
     const id = req.params.id;
